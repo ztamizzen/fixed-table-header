358e5e73aceace959359d4217f378adc5f73ccc9
ztooner <mats.lindblad@gmail.com>
2012-06-07 14:52:47 +0200

    Initial commit

---
+++ js/fixed-table-header.js
@@ -0,0 +1,80 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mali01
+ * Date: 2012-06-07
+ * Time: 12:51
+ */
+(function() {
+    $.fn.fixedTableHeader = function(o) {
+        var options = $.extend({
+                fixedTop: 0,
+                interval: 250
+            }, o),
+            didScroll = false,
+            didResize = false,
+            $win = $(window);
+
+        return this.each(function() {
+            var $this = $(this),
+                $colgroup = $('colgroup', $this),
+                $thead = $('thead', $this),
+                $colgroupClone = $colgroup.clone(false),
+                $theadClone = $thead.clone(false),
+                leftOffset = $this.offset().left,
+                topOffset = $this.offset().top,
+                tableWidth = $this.width(),
+                $tableClone = $('<table></table>');
+
+            $tableClone.addClass('fixed-table-header').css({ margin: '0' });
+            // convert all ID's to className's in the clone to avoid conflicts in the HTML
+            $colgroupClone.find('col[id]').each(function() {
+                $(this).addClass($(this).attr('id'));
+                $(this).removeAttr('id');
+            });
+            $theadClone.css({
+                backgroundColor: $thead.css('background-color')
+            });
+            $tableClone.append($colgroupClone);
+            $tableClone.append($theadClone);
+            $tableClone.css({
+                display: 'none',
+                left: leftOffset,
+                position: 'fixed',
+                top: parseInt(options.fixedTop, 10),
+                width: tableWidth
+            });
+            $this.before($tableClone);
+
+            $win.scroll(function() {
+                didScroll = true;
+            });
+
+            $win.resize(function() {
+                didResize = true;
+            });
+
+            setInterval(function() {
+                if ( didScroll ) {
+                    didScroll = false;
+                    var scrollTop = $win.scrollTop();
+                    if (scrollTop > topOffset) {
+                        $thead.css({ visibility: 'hidden' });
+                        $tableClone.show();
+                    }
+                    else {
+                        $thead.css({ visibility: 'visible' });
+                        $tableClone.hide();
+                    }
+                }
+                if (didResize) {
+                    didResize = false;
+                    // don't parseInt this value because it needs to match exactly
+                    $tableClone.css({ left: $this.offset().left });
+                }
+            }, parseInt(options.interval, 10));
+
+        });
+    };
+
+    $('.fixed-table-header').fixedTableHeader();
+})(jQuery);
